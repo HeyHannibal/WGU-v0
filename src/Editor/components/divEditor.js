92ec2aa37e7f960ee926248acdb3f0c531f605de
heyhannibal <hannibal.inq@gmail.com>
2022-08-05 19:39:26 +0300

    added css units for div size

--- src/Editor/components/divEditor.js
+++ src/Editor/components/divEditor.js
@@ -5,21 +5,43 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import FormControl from "@mui/material/FormControl";
+import { useState } from "react";
 
 export default function DivEditor(props) {
   const { Dom, setDom, findNode, target } = props;
 
   const { editStyle } = useEditor(Dom, setDom, findNode, target.ref);
 
+  let regexp = /px|%|rem/;
+
+  const [units, setUnits] = useState("px");
+
   const divNode = findNode(Dom, target.ref);
   return (
     <div id="editor">
+      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+        <InputLabel id="demo-select-small">Units</InputLabel>
+        <Select
+          labelId="demo-select-small"
+          id="demo-select-small"
+          value={units}
+          label="Age"
+          onChange={(e) => setUnits(e.target.value)}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={"px"}>px</MenuItem>
+          <MenuItem value={"%"}>%</MenuItem>
+          <MenuItem value={"rem"}>rem</MenuItem>
+        </Select>
+      </FormControl>
       <TextField
         id="outlined-number"
         label="H"
         type="number"
-        value={divNode.style.height ? divNode.style.height.split("p")[0] : 30}
-        onChange={(e) => editStyle("height", e.target.value + "px")}
+        value={divNode.style.height ? divNode.style.height.split(regexp)[0] : 30}
+        onChange={(e) => editStyle("height", e.target.value + units)}
         InputLabelProps={{
           shrink: true,
         }}
@@ -28,8 +50,8 @@ export default function DivEditor(props) {
         id="outlined-number"
         label="W"
         type="number"
-        value={divNode.style.width ? divNode.style.width.split("p")[0] : 30}
-        onChange={(e) => editStyle("width", e.target.value + "px")}
+        value={divNode.style.width ? divNode.style.width.split(regexp)[0] : 30}
+        onChange={(e) => editStyle("width", e.target.value + units)}
         InputLabelProps={{
           shrink: true,
         }}
